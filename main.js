de55e5608ed6b9ddea5446b8254e9c9873f4ea14
deveshdanai <deveshdanai@gmail.com>
2021-06-01 21:47:56 +0530

    Initial commit

--- main.js
+++ main.js
@@ -15,18 +15,6 @@ window.addEventListener('contextmenu', function (e) {
 
 
 
-//paste this code under the head tag or in a separate js file.
-	// Wait for window load
-	$(window).load(function() {
-		// Animate loader off screen
-		$(".se-pre-con").fadeOut("slow");;
-	});
-
-
-
-
-	// 
-
 
 
 
